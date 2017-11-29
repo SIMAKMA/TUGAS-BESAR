7db6b3e6c5c5d0b4dc60983a3f53f12e6426048e
darulkamal <16523190@students.uii.ac.id>
2017-11-29 14:21:25 +0700

    tugas

---
+++ tugas besar/page/penduduk/penduduk.php
@@ -0,0 +1,23 @@
+ <div class="row">
+                <div class="col-md-12">
+                    <!-- Advanced Tables -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                             Advanced Tables
+                        </div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>NIK</th>
+                                            <th>Nama</th>
+                                            <th>Alamat</th>
+                                            <th>Pekerjaan</th>
+                                            <th>Pendapatan</th>
+                                            <th>Jumlah Anggota keluarga</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
