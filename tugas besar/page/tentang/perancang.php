6884141a80fb7ab4e2ac2e2b496aa6763bae6519
Fadli Rahmat Ihsan <16523034@students.uii.ac.id>
2017-12-12 22:57:17 +0700

    Add files via upload

---
+++ tugas besar/page/tentang/perancang.php
@@ -0,0 +1,35 @@
+<div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     <h2 style="color: black;">DEVELOPER</h2>   
+                    
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                  <hr />
+                <div class="row">
+                <div class="col-md-3 col-sm-6 col-xs-6">           
+			<div class="panel panel-back noti-box">
+                <img src="assets/img/hill.jpg" class="user-image img-responsive"">
+                <div class="text-box" >
+                    <p class="text-muted">MUSTAFA KAMAL</p>
+                </div>
+             </div>
+		     </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">           
+			<div class="panel panel-back noti-box">
+                <img src="assets/img/inyong.jpg" class="user-image img-responsive">
+                <div class="text-box" >
+                    <p class="text-muted">Fadli Rahmat Ihsan</p>
+                </div>
+             </div>
+		     </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">           
+			<div class="panel panel-back noti-box">
+                <img src="assets/img/find_user.png" class="user-image img-responsive">
+                <div class="text-box" >
+                    <p class="text-muted">Galih Jufa W L</p>
+                </div>
+             </div>
+		     </div>
+			</div>
